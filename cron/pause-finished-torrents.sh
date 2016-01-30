2f70b3ae2281a7ac57d31c64634a27b3f1622cfd
Liem Dinh <flipsorry169@gmail.com>
2016-01-30 11:16:22 -0800

    removed unecessary files

--- cron/pause-finished-torrents.sh
+++ cron/pause-finished-torrents.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl "http://localhost/v1/ajax/torrent/utorr/pauseFinished.php" >> /home/flipsorry/cron/pausedTorrents.txt
+curl "http://localhost/v1/ajax/torrent/utorr/pauseFinished.php" >> /home/flipsorry/cron/logs/pausedTorrents.txt
 exit
