a793e5440b9e5cef21bb50e5839c7a93a3a785f0
Liem Dinh <flipsorry169@gmail.com>
2016-01-30 16:39:10 -0800

    reading from mysql

--- cron/execute-command.sh
+++ cron/execute-command.sh
@@ -1,25 +1,51 @@
 #!/bin/bash
 
-files=/home/flipsorry/cron/runjob/*
+jobs=`mysql --user=root --password=liemdinh Torrents --silent --skip-column-names -e "SELECT Title, InputFile, OutputFile, LogFile, Type, Status FROM ConvertVideo WHERE Status = 'ENQUEUE' LIMIT 1;"`
 
-for file in $files
+IFS=$'\n'
+echo $IFS
+for job in $jobs
 do
-  echo "Processing $file file..."
-  # take action on each file. $f store current file name
-  inputFile=`cat $file | jq --raw-output '.inputFile'`
-  outputFile=`cat $file | jq --raw-output '.outputFile'`
-  logFile=`cat $file | jq --raw-output '.logFile'`
-  additionalArgs=`cat $file | jq --raw-output '.additionalArgs'`
+  #echo "JOBS: $job"
+  title=`echo "$job" | awk -F'\t' '{print $1}'`
+  inputFile=`echo "$job" | awk -F'\t' '{print $2}'`
+  outputFile=`echo "$job" | awk -F'\t' '{print $3}'`
+  logFile=`echo "$job" | awk -F'\t' '{print $4}'`
+  typeFile=`echo "$job" | awk -F'\t' '{print $5}'`
+  #outputFile=`cat $file | jq --raw-output '.outputFile'`
+  #logFile=`cat $file | jq --raw-output '.logFile'`
+  #additionalArgs=`cat $file | jq --raw-output '.additionalArgs'`
+  echo "title: $title"
   echo "inputFile: $inputFile"
   echo "outputFile: $outputFile"
   echo "additionalArgs: $additionalArgs"
   echo "logFile: $logFile"
-  rm "$file"
+  mysql --user=root --password=liemdinh Torrents -e "UPDATE ConvertVideo Set Status = 'CONVERTING' WHERE Title ='$title'"
+  avconv -y -i "$inputFile" -vcodec copy -acodec aac -strict experimental "$outputFile" 2> $logFile
+  mysql --user=root --password=liemdinh Torrents -e "UPDATE ConvertVideo Set Status = 'DONE' WHERE Title ='$title'"
+
+done
+
+files=/home/flipsorry/cron/runjob/*
+
+#for file in $files
+#do
+  #echo "Processing $file file..."
+  # take action on each file. $f store current file name
+  #inputFile=`cat $file | jq --raw-output '.inputFile'`
+  #outputFile=`cat $file | jq --raw-output '.outputFile'`
+  #logFile=`cat $file | jq --raw-output '.logFile'`
+  #additionalArgs=`cat $file | jq --raw-output '.additionalArgs'`
+  #echo "inputFile: $inputFile"
+  #echo "outputFile: $outputFile"
+  #echo "additionalArgs: $additionalArgs"
+  #echo "logFile: $logFile"
+  #rm "$file"
   #inputFile="/home/flipsorry/Dos/Torrents/Sicario/Sicario (2015).mkv"
   #echo $inputFile
   #ls $inputFile
-  avconv -y -i "$inputFile" -vcodec copy -acodec aac -strict experimental "$outputFile" 2> $logFile
+  #avconv -y -i "$inputFile" -vcodec copy -acodec aac -strict experimental "$outputFile" 2> $logFile
   #$command
-done
+#done
 
 exit
